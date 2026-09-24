76b1183e6dd098e257a657491f970a417ad76295
Varun Choudhary <varunchoudhary11223344@gmail.com>
2026-09-24 14:53:23 +0530

    spell error fixed

--- app/core/config.py
+++ app/core/config.py
@@ -14,7 +14,7 @@ class Settings(BaseSettings):
     celery_result_backend: str
 
     jwt_secret_key: str
-    hwt_algorithm: str = "HS256"
+    jwt_algorithm: str = "HS256"
     jwt_access_token_expire_minutes: int = 30
 
     api_key_pepper: str
